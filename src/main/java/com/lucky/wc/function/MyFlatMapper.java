9950e3b9bec35e7ab9866e7c8002aa3174b550cc
xiaoran04 <xiaoran04@meituan.com>
2021-06-24 20:20:46 +0800

    format

--- src/main/java/com/lucky/wc/function/MyFlatMapper.java
+++ src/main/java/com/lucky/wc/function/MyFlatMapper.java
@@ -10,7 +10,7 @@ public class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Inte
         String[] arr = s.split(" ");
         //遍历所有word，组合成二元组输出
         for (String word : arr) {
-            collector.collect(new Tuple2<>(word,1));
+            collector.collect(new Tuple2<>(word, 1));
         }
     }
 }
